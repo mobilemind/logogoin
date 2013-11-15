cececcbe9b260c16486f0e7c28e91be2da71c1a2
Tom King <mobilemind@pobox.com>
2013-11-14 23:32:49 -0800

    src/logogoin.js, web/logogoin.js:update GoGo signIn URL; click submit button instead of form.submit()

--- src/logogoin.js
+++ src/logogoin.js
@@ -2,10 +2,10 @@
 // complete login form if current page is the login page
 // else open that page
 var u, p, d = document;
-if (/^https:\/\/airborne\.gogoinflight\.com\/./.test(location.href)) {
+if (/^https:\/\/airborne\.gogoinflight\.com\/gbp\/signInsignUp.*/.test(location.href)) {
   d.getElementById('returningRadio').checked = true;
   d.getElementById('loginEmail').value = u ? u : window.prompt('Username/email:','');
   d.getElementById('loginPassword').value = p ? p: window.prompt('Password:','');
-  d.forms[0].submit();
+  d.getElementsByName('_eventId_next')[1].click();
 }
-else location.href = 'https://airborne.gogoinflight.com/gbp/signInsignUp.do?execution=e2s1';
+else location.href = 'https://airborne.gogoinflight.com/gbp/signInsignUp.do?login=true';
