e21760a801484a6f26ead84d7bcd3392108cfbc0
Tom King <mobilemind@pobox.com>
2017-03-14 12:16:47 -0400

    src/logogoin.js: use ES6 const instead of var; minor eslint tweaks

--- src/logogoin.js
+++ src/logogoin.js
@@ -1,11 +1,12 @@
 /* jshint strict: true */
 // complete login form if current page is the login page
 // else open that page
-var u, p, d = document;
+const d = document, p = '', u = '';
 if (/^https:\/\/airborne\.gogoinflight\.com\/gbp\/signInsignUp.*/.test(location.href)) {
   d.getElementById('returningRadio').checked = true;
   d.getElementById('loginEmail').value = u ? u : window.prompt('Username/email:','');
-  d.getElementById('loginPassword').value = p ? p: window.prompt('Password:','');
+  d.getElementById('loginPassword').value = p ? p : window.prompt('Password:','');
   d.getElementsByName('_eventId_next')[1].click();
+} else {
+  location.href = 'https://airborne.gogoinflight.com/gbp/signInsignUp.do?login=true';
 }
-else location.href = 'https://airborne.gogoinflight.com/gbp/signInsignUp.do?login=true';
